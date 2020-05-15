f942e6ea9bd9d0d3a22a93ac78bd58a883f0a426
Remi-WANG <57351435+Remi-WANG@users.noreply.github.com>
2020-05-15 10:42:20 +0800

    Update 提取截面数据.sql

--- 提取截面数据.sql
+++ 提取截面数据.sql
@@ -1,4 +1,5 @@
 SHOW OPEN TABLES FROM cn_stock_quote WHERE `in_use`!=0;
+#不加`` 直接写database名称也可以
 USE `cn_stock_quote`;
 SELECT code,shortname,trade_date,EXCHANGE,pre_close,OPEN,volume,high,low,LAST
 FROM daily_quote
